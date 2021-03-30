b2166cb231eed9931f349538dfd6120f0221d349
jensengbg-jack-carling <72305598+jensengbg-jack-carling@users.noreply.github.com>
2021-03-30 13:11:38 +0200

    Update Menu.vue and Store

--- airbean/src/views/Menu.vue
+++ airbean/src/views/Menu.vue
@@ -1,20 +1,27 @@
 <template>
   <main>
     <img class="svg header" src="@/assets/graphics-header.svg" alt="">
-        <section class="navigation">
-          <NavigationButton /> 
-        </section>
+          <NavigationButton class="navigation" />
+          <CartButton class="cart" />
         <h1>Meny</h1>
+        <MenuItems />
     <img class="svg footer" src="@/assets/graphics-footer.svg" alt="">
   </main>
 </template>
 
 <script>
 import NavigationButton from '../components/NavigationButton.vue'
+import CartButton from '../components/CartButton.vue'
+import MenuItems from '../components/MenuItems.vue'
 
 export default {
   components: {
-    NavigationButton
+    NavigationButton,
+    CartButton,
+    MenuItems
+  },
+  created() {
+    this.$store.dispatch('getMenu');
   }
 }
 </script>
@@ -22,25 +29,33 @@ export default {
 <style scoped>
 main {
   width: 100%;
-  height: 100%;
-  z-index: 1;
+  min-height: 100%;
+  background-color: #F3E4E1;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #F3E4E1;
+  position: relative;
 }
-section.navigation {
-  display: flex;
-  justify-content: space-between;
+.navigation {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.cart {
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 h1 {
   font-family: 'PT Serif', serif;
   font-weight: 700;
   font-size: 2.6rem;
-  margin: 2.5rem 0;
+  margin-top: 120px;
+  color: #2F2926;
 }
 img.svg {
   position: absolute;
+  width: 100%;
 }
 img.svg.header {
   top: 0;
