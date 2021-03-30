6b563f10d1da94abee89b9aea7f0a6c8612c7ffb
jensengbg-Marijune-Lejsund <59868177+jensengbg-Marijune-Lejsund@users.noreply.github.com>
2021-03-30 13:00:51 +0200

    Create Navigation.vue

---
+++ airbean/src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-page">
+      <img src="../assets/close.svg" alt="" class="close-button">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.nav-page{
+    width: 375px;
+    height: 667px;
+    background: #2F2926;
+}
+
+</style>
