5c7b1489c15412857569c9452f3fa76fe0e22462
Padma <padmaprogrammer@gmail.com>
2021-03-31 08:58:59 +0200

    Updated Profile feature
    Not complete.. need to sync with API and store.js

---
+++ airbean/src/components/CreateProfile.vue
@@ -0,0 +1,68 @@
+
+
+<template>
+  <div class="createProfile">
+    <img class="logo__dark" src="@/assets/airbean-logo-dark.svg" />
+    <h1 class="header__main">Välkommen till AirBean-familjen!</h1>
+    <p>Genom att skapa ett konto nedan kan du spara och se din orderhistorik</p>
+    <section class="signup__form">
+      <input
+        class="signup__form-text-input"
+        type="text"
+        name="name"
+        placeholder="Namn"
+        v-model="profile.name"
+      />
+
+      <input
+        class="signup__form-text-input"
+        type="text"
+        name="email"
+        placeholder="epost@exempel.com"
+        v-model="profile.email"
+      />
+      <p>GDPR Ok!</p>
+
+      <input type="radio" class="okGDPR" v-model="GDPR" />
+      <button @click="saveProfile">Logga in</button>
+    </section>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      profile: {
+        name: "",
+        email: "",
+      },
+      gdpr: false,
+    };
+  },
+  saveProfile() {
+    this.$store.dispatch("setProfile", this.profile);
+    this.$store.dispatch("toggleSignUp");
+  },
+};
+</script>
+
+<style scoped>
+.createProfile {
+  margin-top: 82px;
+  position: fixed;
+  z-index: 900;
+  background-color: #3fe20d; /*Yikes*/
+  border-radius: 3%;
+  border: 2px, solid #2f2926;
+  height: 100vh;
+  width: 341px;
+}
+
+.signup__form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
--- airbean/src/views/Profile.vue
+++ airbean/src/views/Profile.vue
@@ -1,13 +1,215 @@
 <template>
-  <h1>Profile.vue</h1>
+  <section class="main__wrapper">
+    <section class="header">
+      <NavigationButton class="navigation" />
+      <img class="graphics__header" src="@/assets/graphics-header.svg" />
+    </section>
+
+    <div class="profile__details">
+      <img class="display__image" src="@/assets/profile.svg" />
+
+      <h1 class="header__main">{{ profile.name }}</h1>
+      <!-- {{ profile.name }} --->
+      <!-- Sixten Kaffelöver --->
+      <h2 class="profile__email">{{ profile.email }}</h2>
+      <!-- {{ profile.email }} --->
+      <!-- sixten.kaffelover@zocom.se --->
+    </div>
+
+    <createProfile v-show="openSignUp" />
+
+    <h1 class="header__main--space">Orderhistorik</h1>
+
+    <li
+      class="purchase__history"
+      v-for="purchase in purchaseHistory"
+      :key="purchase.purchaseNum"
+    >
+      <span class="reference__number">{{ purchase.purchaseNumber }}</span>
+      <span class="purchase__date">{{ purchase.date }}</span>
+      <span class="summary__title">total ordersumma</span>
+      <span class="purchase__price">{{ purchase.total }}</span>
+    </li>
+
+    <li class="grand__total">
+      <p>Total spenderat</p>
+      <p>{{ totalCost }}kr</p>
+    </li>
+  </section>
 </template>
 
-<script>
-export default {
 
-}
+<script>
+import NavigationButton from "@/components/NavigationButton.vue";
+import createProfile from "@/components/CreateProfile";
+
+export default {
+  components: {
+    NavigationButton,
+    createProfile,
+  },
+
+  methods: {
+    createProfile() {
+      this.$store.dispatch("toggleSignUp");
+    },
+  },
+  computed: {
+    openSignUp() {
+      return this.$store.state.openSignUp;
+    },
+    profile() {
+      return this.$store.state.profile;
+    },
+    purchaseHistory() {
+      return this.$store.state.purchaseHistory;
+    },
+    totalCost() {
+      return this.$store.getters.totalCost;
+    },
+  },
+  created() {
+    if (this.profile.id) {
+      this.$store.dispatch("getPurchases", this.profile.id);
+    } else {
+      this.$store.dispatch("setId");
+    }
+  },
+};
 </script>
 
 <style scoped>
+.navigation {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
 
+.main__wrapper {
+  background-color: #2f2926;
+  height: 100vh;
+  position: relative;
+}
+
+.header {
+  position: relative;
+}
+
+.profile__details {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.header__main {
+  font-family: "PT Serif", serif;
+  font-weight: 500;
+  color: #ffff;
+  font-size: 24px;
+  line-height: 28.8px;
+  margin-bottom: 0;
+}
+
+.header__main--space {
+  text-align: left;
+  color: #ffff;
+  margin-left: 6%;
+}
+
+.profile__email {
+  font-family: "Work Sans", sans-serif;
+  font-weight: 400;
+  color: #ffff;
+  opacity: 80%;
+  font-size: 14px;
+  line-height: 16.8px;
+}
+
+.past__orders {
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+
+span {
+  font-size: 14px;
+  margin-bottom: 0;
+  color: #ffff;
+  background-color: #2f2926;
+  margin: 0 10%;
+  width: 90%;
+}
+
+.reference__number {
+  font-family: Work Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 17px;
+  letter-spacing: 0em;
+  text-align: left;
+}
+
+.purchase__date {
+  font-family: Work Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 17px;
+  letter-spacing: 0em;
+  text-align: left;
+  margin-bottom: 5%;
+}
+
+.summary__title {
+  font-family: Work Sans;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 14px;
+  letter-spacing: 0em;
+  text-align: left;
+  padding-bottom: 5%;
+  margin-bottom: 5%;
+}
+
+.purchase__price {
+  font-family: Work Sans;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 14px;
+  letter-spacing: 0em;
+  text-align: right;
+  padding-bottom: 5%;
+  margin-bottom: 5%;
+}
+
+span:nth-child(even) {
+  text-align: right;
+  margin-left: 0;
+}
+
+span:nth-child(n + 3):nth-child(-n + 4) {
+  border-bottom: 1px solid rgba(255, 255, 255, 0.08);
+}
+
+span:nth-child(odd) {
+  text-align: left;
+}
+
+.grand__total {
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid rgba(255, 255, 255, 0.6);
+  margin: 5%;
+  flex-direction: row;
+  justify-content: space-between;
+  font-family: Work Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 17px;
+  letter-spacing: 0em;
+  color: #ffff;
+}
 </style>
